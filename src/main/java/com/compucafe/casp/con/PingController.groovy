94eb6f4a458adfca06fc3ae77389c1079ad88126
Chris Hamilton <chamilton-jobs@compucafe.com>
2017-02-08 00:26:54 -0500

    Updating to add property placeholders in Spring and camel contexts

--- src/main/java/com/compucafe/casp/con/PingController.groovy
+++ src/main/java/com/compucafe/casp/con/PingController.groovy
@@ -1,13 +1,18 @@
 package com.compucafe.casp.con
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+import groovy.util.logging.Slf4j
+@Slf4j
 @RestController
 class PingController {
-
+	@Value ('${aValue:NOT_SET}')
+	String aValue
 	@RequestMapping(value='/ping', produces='text/plain')
 	public String ok () {
+		log.debug("ok: we're in. aValue: ${aValue}")
 		'Spring OK\n'
 	}
 }
